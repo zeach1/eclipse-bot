7ea378414f37ed2fe72cbbc2798ff0ba143251ba
Paul Matthew Barrameda <paulmatthewbarrameda@Pauls-MacBook-Pro-2.local>
2018-03-17 21:47:23 -0700

    Update 3/17 - Another format fix
    Also removed .data/config.js
    
    Moved it to data/config.js and merged with parameters.js
    
    All data is private and hidden.

--- commands/jay.js
+++ commands/jay.js
@@ -16,7 +16,7 @@ module.exports = {
       case 'respect':
       case 'fear':
         return messenger.sendMessage(message, {
-          title: arg.charAt(0).toUpperCase() + arg.slice(1) + ' Jay',
+          title: `${arg.charAt(0).toUpperCase()}${arg.slice(1)} Jay`,
           description: arg === 'respect' ? '@everyone respect the dark knight of Go Canada!' : 'You are too weak to take it',
         });
 
@@ -32,7 +32,7 @@ module.exports = {
     if (num == 0) return;
 
     if (message.author.id != user.jay)
-      return message.channel.send('You fear him so much... you cannot try it.');
+      return message.channel.send('You fear him so much... you **[cannot](https://cdn.discordapp.com/attachments/390086345957179393/408337364868530206/trigger.gif)** try it.');
 
     message.channel.send(`<@${user.peril}>`).catch(e => console.log(e));
 
