ad7a7492c4eb8b7cf45ee4ad09e2df3e954e9c60
Paul Matthew Barrameda <paulmatthewbarrameda@Pauls-MacBook-Pro-2.local>
2018-03-19 00:00:39 -0700

    Fixed jay.js

--- commands/jay.js
+++ commands/jay.js
@@ -16,31 +16,11 @@ module.exports = {
       case 'fear':
         return messenger.sendMessage(message, {
           title: `${arg.charAt(0).toUpperCase()}${arg.slice(1)} Jay`,
-<<<<<<< HEAD
-          description: arg === 'respect' ? '@everyone respect the dark knight of Go Canada!' : 'You are too weak to take it',
-=======
           description: arg === 'respect' ? '@everyone respect the dark knight of Go Canada!' : 'You are too weak to take it.',
->>>>>>> glitch
         });
 
       default:
         return messenger.sendArgumentError(message, this, 'Jay does not approve of this argument');
     }
   },
-<<<<<<< HEAD
-
-  irritatePeril: async function(message, num) {
-    if (num == 0) return;
-
-    if (message.author.id != user.jay)
-      return message.channel.send('You fear him so much... you **[cannot](https://cdn.discordapp.com/attachments/390086345957179393/408337364868530206/trigger.gif)** try it.');
-
-    message.channel.send(`<@${user.peril}>`).catch(e => console.log(e));
-
-    setTimeout(() => {
-      return this.irritatePeril(message, num - 1);
-    }, 1000);
-  },
-=======
->>>>>>> glitch
 };
