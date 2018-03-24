b35c7109da6fa2bfd930cf467d05f1ca543d640e
Glitch (eclipse-bot) <none>
2018-03-24 07:17:26 +0000

    ☠️📠 Updated with Glitch - best.js

--- commands/best.js
+++ commands/best.js
@@ -1,16 +1,24 @@
+const { prefix } = require('../data/config.js');
+
 const messenger = require('../helper/messenger.js');
 const playerManager = require('../helper/playerManager.js');
 
 module.exports = {
   name: 'best',
   type: 'essentials',
-  usage: '[exp | ranking] [number]',
+  usage: '<exp | ranking> [number]',
   aliases: ['top'],
   description: 'Gets top players by experience or ranking',
 
+  args: 1,
+  
   execute: async function(message, param) {
     const { args } = param;
-    const type = args[0] === 'ranking' ? 'ranking' : 'exp';
+    const type = args[0];
+    
+    if (type !== 'exp' && type !== 'ranking')
+      return messenger.sendArgumentError(message, this, 'This argument does not exist');
+    
     let number = !isNaN(args[0]) ? parseInt(args[0]) : !isNaN(args[1]) ? parseInt(args[1]) : 10;
 
     const scores = playerManager.getRankList(message, type);
@@ -34,8 +42,8 @@ module.exports = {
       description += `\`${n} ${type === 'exp' ? expLevel : ranking} |\` ${score.flair} ${name}\n`;
     }
 
-    return messenger.sendMessage(message, {
-      title: `🏅 Top Players by ${type === 'exp' ? 'Level' : 'Eclipse Ranking'}`,
+    return messenger.send(message, {
+      title: `🏅 Top Players by ${type === 'exp' ? 'Level' : 'Ranking'}`,
       color: 0xf5f513,
       description: description,
     });
